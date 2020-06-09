901c7b33fb26ea705a7f6e0986bd80aa4862ac1d
MelloTonio <antoniomelloaviao@gmail.com>
2020-06-09 05:01:39 -0300

    feat: ensure SignUpController calls AddAccount with correct values

---
+++ src/domain/usecases/add-account.ts
@@ -0,0 +1,19 @@
+import { AccountModel } from '../models/account'
+
+// The software in this layer contains application specific business rules.
+// It encapsulates and implements all of the use cases of the system.
+// These use cases orchestrate the flow of data to and from the entities,
+// and direct those entities to use their enterprise wide business rules
+// to achieve the goals of the use case.
+
+// What should i receive on my application? Name,email,password -> then another method(AccountModel) should
+// deal with id
+export interface AddAccountModel{
+  name: string
+  email: string
+  password: string
+}
+
+export interface AddAccount {
+  add (account: AddAccountModel): AccountModel
+}
--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,12 +1,15 @@
 import { HttpRequest, HttpResponse, Controller, EmailValidator } from '../protocols'
 import { MissingParamError, InvalidParamError } from '../errors'
 import { badRequest, serverError } from '../helper/http-helper'
+import { AddAccount } from '../../domain/usecases/add-account'
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
-
-  constructor (emailValidator: EmailValidator) {
+  private readonly AddAccount: AddAccount
+  // Como não é responsibilidade desta classe validar o email, separamos as responsabilidades para outra
+  constructor (emailValidator: EmailValidator, addAccount: AddAccount) {
     this.emailValidator = emailValidator
+    this.AddAccount = addAccount
   }
 
   handle (httpRequest: HttpRequest): HttpResponse {
@@ -19,7 +22,7 @@ export class SignUpController implements Controller {
           return badRequest(new MissingParamError(field))
         }
       }
-      const { email, password, passwordConfirmation } = httpRequest.body
+      const { name, email, password, passwordConfirmation } = httpRequest.body
       if (password !== passwordConfirmation) {
         return badRequest(new InvalidParamError('passwordConfirmation'))
       }
@@ -28,6 +31,11 @@ export class SignUpController implements Controller {
       if (!isValid) {
         return badRequest(new InvalidParamError('email'))
       }
+      this.AddAccount.add({
+        name,
+        email,
+        password
+      })
     } catch (error) {
       return serverError()
     }
